420c508210abc8e0480b0081f7e34c7e771c89c1
MR-Design <two7city@gmail.comMR-Design>
2018-09-26 16:17:51 -0500

    Added Methode To Customize Recipe git add . Next Will be Cost of Recipe

--- _Lemonade_Stand/_Lemonade_Stand/Inventory.cs
+++ _Lemonade_Stand/_Lemonade_Stand/Inventory.cs
@@ -8,31 +8,31 @@ namespace _Lemonade_Stand
 {
     public class Inventory
     {
-        //   I want the basic Lemonade Stand gameplay to be present
-        //  double Wallet = 20.00;
-        //  int Lemons = 0; int Sugar = 0; int IceCubes = 0;
+        public double lemons = 100/1000; public double sugar = 50 / 1000; public double iceCubes = 25 / 1000;
+        double CostOfRecipe; int Nlemons; int Nsugar; int NiceCubes;
 
-
-    
-       //  the ability to make a recipe for my lemonade, 
-      // so that I can include x-amount of lemons, x-amount of sugar, and x-amount of ice.
-      public void SetRecipe(int lemons = 4, int sugar = 4, int iceCubes = 4)
+        public void GetCostOfRecipe()
         {
-             Console.WriteLine("Your Curent Recipe Is Basic" + lemons + " : Lemons" + sugar + " : Sugars" + iceCubes + " : Ice Cubes");
-             Console.WriteLine("You Have a Basic Lemonade Recipe : Tape [YES] If you Like To Make a New Reicipe!!");
-             string CustomRecipe =Console.ReadLine();
-             Console.WriteLine(CustomRecipe.ToUpper());
+            CostOfRecipe = (Nlemons * lemons) + (Nsugar * sugar) + (NiceCubes * iceCubes);
+        }
 
-                if (CustomRecipe == "YES")
-                {
+        public void MakeRecipe()
+        {
+            Console.WriteLine("Alright let's Make a New Recipe");
 
-                }
+            Console.WriteLine("Enter How Many Lemens you Would Like");
+            Nlemons = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Enter How Many Sugar you Would Like");
+            Nsugar = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Enter How Many Lemens you Would Like");
+            NiceCubes = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Here's Your Recipe "+Nlemons+" lemons "+ Nsugar+" sugar And  "  +NiceCubes+ " iceCubes");
 
 
         }
-
 
     }
 }
--- _Lemonade_Stand/_Lemonade_Stand/Player.cs
+++ _Lemonade_Stand/_Lemonade_Stand/Player.cs
@@ -8,5 +8,32 @@ namespace _Lemonade_Stand
 {
     class Player
     {
+        Inventory inventory = new Inventory();
+        //   I want the basic Lemonade Stand gameplay to be present
+        //  double Wallet = 20.00;
+        //  int Lemons = 0; int Sugar = 0; int IceCubes = 0;
+
+
+
+        //  the ability to make a recipe for my lemonade, 
+        // so that I can include x-amount of lemons, x-amount of sugar, and x-amount of ice.
+         int lemons = 4;  int sugar = 4;  int iceCubes = 4;
+        public void SetRecipe()
+        {
+            Console.WriteLine("Your Curent Recipe Is Basic" + lemons + " : Lemons" + sugar + " : Sugars" + iceCubes + " : Ice Cubes");
+            Console.WriteLine("You Have a Basic Lemonade Recipe : Tape [YES] If you Like To Make a New Reicipe!!");
+            string CustomRecipe = Console.ReadLine();
+            Console.WriteLine(CustomRecipe.ToUpper());
+
+            if (CustomRecipe == "YES")
+            {
+                // Function MakeRecipe.
+                inventory.MakeRecipe();
+            }
+
+
+
+
+        }
     }
 }
